f5d11f4aaa1a94f6b4702dd82c936ac07e9dd5f3
Alexandr Nikishin <wisdoomer@gmail.com>
2016-04-03 18:15:26 +0300

    Optimize expamples

--- Example/WSTableKit/WSViewController.m
+++ Example/WSTableKit/WSViewController.m
@@ -16,6 +16,7 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
+@property (strong, nonatomic) WSSection *firstSection;
 @property (nonatomic, strong) WSSectionContainer *sections;
 
 @end
@@ -31,42 +32,44 @@
     
     NSArray *cells = [WSCellItem itemsWithClass:[CellWithButton class] objects:@[@"One", @"Two", @"Three"] customActions:@[action]];
 
-    WSSection *section = [[WSSection sectionWithItems:cells tableView:self.tableView] setAdjustmentBlock:^(CellWithButton *cell, WSCellItem * _Nonnull item, NSIndexPath * _Nonnull path) {
+    self.firstSection = [[WSSection sectionWithItems:cells tableView:self.tableView] setAdjustmentBlock:^(CellWithButton *cell, WSCellItem * _Nonnull item, NSIndexPath * _Nonnull path) {
         cell.textLabel.text = item.object;
         cell.textLabel.backgroundColor = [UIColor clearColor];
         cell.bottomSeparatorHidden = cell.topSeparatorHidden = NO;
         cell.bottomSeparatorInsets = UIEdgeInsetsMake(0, 4, 1, 5);
         cell.topSeparatorInsets = UIEdgeInsetsMake(1, 4, 0, 5);
     }];
-    section.sectionHeader = [WSSupplementaryItem itemWithTitle:@"Test" height:22];
+    self.sections = [WSSectionContainer containerWithSections:@[self.firstSection] tableView:self.tableView];
+    
+    [self configureHeaderFooter];
+    [self configureActions];
+}
+
+- (void)configureHeaderFooter {
+    self.firstSection.sectionHeader = [WSSupplementaryItem itemWithTitle:@"Test" height:22];
     UILabel *label = [UILabel new];
     label.text = @"Footer";
-    section.sectionFooter = [WSSupplementaryItem itemWithClass:[WSHeaderFooterView class] object:label height:40];
-    [[section itemAtIndex:0] addAction:[WSAction actionWithType:WSActionWillDisplay actionBlock:^(WSActionInfo * _Nonnull actionInfo) {
+    self.firstSection.sectionFooter = [WSSupplementaryItem itemWithClass:[WSHeaderFooterView class] object:label height:40];
+}
+
+- (void)configureActions {
+    [[self.firstSection itemAtIndex:0] addAction:[WSAction actionWithType:WSActionWillDisplay actionBlock:^(WSActionInfo * _Nonnull actionInfo) {
         actionInfo.cell.contentView.backgroundColor = [UIColor whiteColor];
         [UIView animateWithDuration:3.f animations:^{
             actionInfo.cell.contentView.backgroundColor = [UIColor blueColor];
         }];
     }]];
-    [[section itemAtIndex:1] addAction:[WSAction actionWithType:WSActionClick actionBlock:^(WSActionInfo * _Nonnull actionInfo) {
+    [[self.firstSection itemAtIndex:1] addAction:[WSAction actionWithType:WSActionClick actionBlock:^(WSActionInfo * _Nonnull actionInfo) {
         NSLog(@"Cell clicked");
     }]];
-    [[section itemAtIndex:1] addAction:[WSAction actionWithType:WSActionWillSelect returnValueBlock:^(WSActionInfo * _Nonnull actionInfo) {
+    [[self.firstSection itemAtIndex:1] addAction:[WSAction actionWithType:WSActionWillSelect returnValueBlock:^(WSActionInfo * _Nonnull actionInfo) {
         NSLog(@"Cell will selected");
         return actionInfo.path;
     }]];
-    [[section itemAtIndex:2] addAction:[WSAction actionWithType:WSActionSelect returnValueBlock:^(WSActionInfo * _Nonnull actionInfo) {
+    [[self.firstSection itemAtIndex:2] addAction:[WSAction actionWithType:WSActionSelect returnValueBlock:^(WSActionInfo * _Nonnull actionInfo) {
         NSLog(@"Cell selected");
         return actionInfo.path;
     }]];
-    
-    self.sections = [WSSectionContainer containerWithSections:@[section] tableView:self.tableView];
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    
 }
 
 @end
