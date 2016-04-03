b53e8d06f6e5eea47e9bc0def060191e446d8ef4
Alexandr Nikishin <wisdoomer@gmail.com>
2016-04-03 18:11:20 +0300

    Optimise adjustment

--- WSTableKit/WSSection.m
+++ WSTableKit/WSSection.m
@@ -120,7 +120,9 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     if (_sectionHeader && [_items count] != 0) {
         UITableViewHeaderFooterView<WSCellClass> *header = [_cellPrototyper headerFooterPrototypeForCellClass:_sectionHeader.viewClass];
-        [_sectionHeader.adjustment invokeActionWithInfo:[WSActionInfo actionInfoWithView:header item:_sectionHeader path:nil userInfo:nil]];
+        if (_sectionHeader.adjustment) {
+            [_sectionHeader.adjustment invokeActionWithInfo:[WSActionInfo actionInfoWithView:header item:_sectionHeader path:nil userInfo:nil]];
+        }
         [header applyItem:_sectionHeader heightCalculation:YES];
         return ([header respondsToSelector:@selector(cellHeight)]) ? [header cellHeight] : tableView.sectionHeaderHeight;
     }
@@ -131,6 +133,9 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
     if (_sectionFooter && [_items count] != 0) {
         UITableViewHeaderFooterView<WSCellClass> *footer = [_cellPrototyper headerFooterPrototypeForCellClass:_sectionFooter.viewClass];
+        if (_sectionFooter.adjustment) {
+
+        }
         [_sectionFooter.adjustment invokeActionWithInfo:[WSActionInfo actionInfoWithView:footer item:_sectionFooter path:nil userInfo:nil]];
         [footer applyItem:_sectionFooter heightCalculation:YES];
         return ([footer respondsToSelector:@selector(cellHeight)]) ? [footer cellHeight] : tableView.sectionFooterHeight;
@@ -144,7 +149,9 @@
     if (_sectionHeader) {
         NSString *identifier = [[_cellPrototyper identifierConvention] identifierForClass:_sectionHeader.viewClass];
         header = [_tableView dequeueReusableHeaderFooterViewWithIdentifier:identifier];
-        [_sectionHeader.adjustment invokeActionWithInfo:[WSActionInfo actionInfoWithView:header item:_sectionHeader path:nil userInfo:nil]];
+        if (_sectionHeader.adjustment) {
+            [_sectionHeader.adjustment invokeActionWithInfo:[WSActionInfo actionInfoWithView:header item:_sectionHeader path:nil userInfo:nil]];
+        }
         [header applyItem:_sectionHeader heightCalculation:NO];
     }
     
@@ -156,7 +163,9 @@
     if (_sectionFooter) {
         NSString *identifier = [[_cellPrototyper identifierConvention] identifierForClass:_sectionFooter.viewClass];
         footer = [_tableView dequeueReusableHeaderFooterViewWithIdentifier:identifier];
-        [_sectionFooter.adjustment invokeActionWithInfo:[WSActionInfo actionInfoWithView:footer item:_sectionFooter path:nil userInfo:nil]];
+        if (_sectionFooter.adjustment) {
+            [_sectionFooter.adjustment invokeActionWithInfo:[WSActionInfo actionInfoWithView:footer item:_sectionFooter path:nil userInfo:nil]];
+        }
         [footer applyItem:_sectionFooter heightCalculation:NO];
     }
     
